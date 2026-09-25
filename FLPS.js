c4549fd9b84d618e7a1a2af7e445a73bc6ac0369
brenuzy <victor.andrade.costa@escola.pr.gov.br>
2026-09-25 08:42:52 -0300

    25/09 18th build

---
+++ FLPS.js
@@ -0,0 +1,12 @@
+let idade = 16;
+
+if (idade === 16) {
+  console.log("A pessoa tem 16 anos e ainda não pode dirigir.");
+} else if (idade >= 18) {
+  console.log("A condução é permitida.");
+} else {
+  console.log("A pessoa ainda não pode dirigir.");
+}
+A pessoa tem 16 anos e ainda não pode dirigir.
+let idade = 18;
+A condução é permitida.
